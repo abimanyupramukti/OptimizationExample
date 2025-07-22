33e10639643f0a0257b14834155a330b65b77a2c
Abimanyu Pramukti <abimanyupramukti@yahoo.com>
2025-07-22 18:52:17 +0700

    add missing files

---
+++ OptimizationExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OptimizationExample
+//
+//  Created by Abimanyu Pramukti on 22/07/25.
+//
+
+import UIKit
+import ExampleModule
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemGreen
+        ExampleModule.main()
+    }
+}
+
